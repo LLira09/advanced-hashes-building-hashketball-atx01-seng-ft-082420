ecb1685eac7d8acfb39202e755b7976d8ca28a26
Jake <llira09@gmail.com>
2020-07-31 17:26:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,20 @@
 def game_hash
   {
-    
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        :player_name => "",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => "",
+        ]
+    },
+    :away => {}
   }
 end
